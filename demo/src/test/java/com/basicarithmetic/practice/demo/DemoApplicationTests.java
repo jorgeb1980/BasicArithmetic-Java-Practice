b05eba4f2cc31f6c201d1658241e80b535983f1e
PLeon6822 <pablo.leon@brandcrumb.com>
2019-09-16 17:29:59 +0200

    Added unit tests for Service

--- demo/src/test/java/com/basicarithmetic/practice/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/basicarithmetic/practice/demo/DemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.basicarithmetic.practice.demo;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	BasicService testService;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testAdd() {
+		assertEquals(3, testService.add(1,2));
+	}
+
+	@Test
+	public void testSubtract() {
+		assertEquals(-1, testService.subtract(1,2));
+	}
+
+	@Test
+	public void testProduct() {
+		assertEquals(20, testService.product(10,2));
+	}
+
+	@Test
+	public void testDivision() {
+		HashMap<String,Integer> answer = new HashMap<>();
+		answer.put("quotient",2);
+		answer.put("remainder",1);
+		assertEquals(answer, testService.division(7,3));
+	}
 }
